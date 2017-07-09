b214e4a25b6372755afb7247ac8ab911bdd15dae
Jorrit Visser <jorritjanvisser@gmail.com>
2017-07-09 18:08:51 +0200

    Final version - cleaned up, POSIX, commented
    * Add proper crediting
    * Added —language option
    * Comments moved inline
    * POSIX
      * Replaced array with string
      * Use literal newline in sed
      * [ instead of [[

--- shiki.sh
+++ shiki.sh
@@ -1,58 +1,81 @@
 # Wikipedia in the shell
-# https://www.mediawiki.org/wiki/API:Main_page
-# https://www.mediawiki.org/wiki/Extension:TextExtracts
-# For `formatversion=1` use `sed 's/"}}}.*//'
-# Usage: `wiki unix` or `wiki -l "Ghost in the Shell"`
-# Options: -l(ong) and -s(hort)
-# Major props to konsolebox
+# © Jorrit Visser <git.io/jorvi>
+# Major props to konsolebox <https://git.io/konsolebox>
 
-wiki() {
-  local data=() encoded_data lang long_mode=false simple_mode=false url
+# Usage: `shiki unix`, `shiki -f "Ghost in the Shell" --language fr`
+# Options: `-s/--simple`, `-f/--full` and `$article -l/--language`
+# '--simple' and '--language' can't be used together
 
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-    -l|--long)
-      long_mode=true
-      ;;
-    -s|-simple)
+shiki() {
+  local article="" encoded_article lang="" url full_mode=false simple_mode=false
+
+  while [ "${#}" -gt 0 ]; do
+    case "${1}" in # Check if first argument is an option
+    -f|--full)
+      full_mode=true # request full article
+    ;;
+    -s|--simple)
+      simple_mode=true # request simple article
+    ;;
+    -fs|-sf|--simple-full|--full-simple)
+      full_mode=true
       simple_mode=true
-      ;;
-    -ls|-sl|-s -l|-l -s|--simple-long|--long-simple)
-      long_mode=true
-      simple_mode=true
-      ;;
-    -*)
-      echo "Invalid option: $1"
-      return 1
-      ;;
+    ;;
+    -l|--language)
+      lang="${2}"
+      shift # Use shift to eat argument
+    ;;
     --)
-      data+=("${@:2}")
-      break
-      ;;
+      shift
+      if [ "${#}" -gt 0 ] && [ -n "${article}" ]; then
+        printf "You can't request multiple articles!\n"
+        return 1
+      fi
+      article="${1}"
+      break # break out of `case` to prevent 'article' reassignment
+    ;;
+    -*)
+      printf "Invalid option: ${1}!\n"
+      return 1
+    ;;
     *)
-      data+=("$1")
-      ;;
+      if [ -n "${article}" ]; then
+        printf "You can't request multiple articles!\n"
+        return 1
+      fi
+      article="${1}"
+    ;;
     esac
 
     shift
   done
 
-  case ${#data[@]} in
-  0)
-    echo "Data argument needed."
+  # Check if article string is set
+  if [ -z "${article}" ]; then
+    printf "You must specify an article!\n"
     return 1
-    ;;
-  1)
-    ;;
-  *)
-    echo "Too many data arguments passed."
-    return 1
-    ;;
-  esac
+  fi
 
-  encoded_data=$(python -c "import urllib, sys; print urllib.quote(sys.argv[1])" "$data") || return 1
-  if [[ $simple_mode == true ]]; then lang="simple"; else lang="en"; fi;
-  url="https://${lang}.wikipedia.org/w/api.php?action=query&titles=${encoded_data}&prop=extracts&exlimit=max&explaintext&format=json&formatversion=2&redirects="
-  [[ $long_mode == true ]] || url+="&exintro"
-  curl -fsSL "$url" | sed 's/^.*"extract":"//; s/"}\]}.*//; s/\\n/\'$'\n''/g' | less
+  # URL-encode article string
+  encoded_article="$(python -c "import urllib, sys; print urllib.quote(sys.argv[1])" "${article}")" || return 1
+
+  if [ -n "${lang}" ] && [ "${simple_mode}" = true ]; then
+    printf "'--simple' and '--language' can't be used together!\n" # 'simple' is considered a language
+    return 1
+  elif [ "${simple_mode}" = true ]; then
+    lang="simple"
+  elif [ -z "${lang}" ]; then
+    lang="en" # if language is not set, default to English
+  fi;
+
+  # https://www.mediawiki.org/wiki/API:Main_page
+  # https://www.mediawiki.org/wiki/Extension:TextExtracts
+  url="https://${lang}.wikipedia.org/w/api.php?action=query&titles=${encoded_article}&prop=extracts&exlimit=max&explaintext&format=json&formatversion=2&redirects="
+  [ "${full_mode}" = true ] || url="${url}&exintro" # Full or short article
+
+  # Remove all markup cruft at article boundaries, with `sed`
+  # For `formatversion=1` use `sed 's/"}}}.*//'`
+  # Literal newline `sed` hack for POSIX compatibility
+  curl -fsSL "${url}" | sed 's/^.*"extract":"//; s/"}\]}.*//; s/\\n/\
+/g' | less # Pipe into `less` for cleaner article browsing
 }
